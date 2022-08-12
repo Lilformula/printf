6d7fc64b71f98837ab78e996653786e286c6879a
Ben-Gurion Zanus <106863674+Lilformula@users.noreply.github.com>
2022-08-12 12:23:39 +0000

    Update convert_hex.c

--- convert_hex.c
+++ convert_hex.c
@@ -1,4 +1,10 @@
-#include "main.h"
+/*
+ * File: convert_hex.c
+ * Auth: Brennan D Baraban
+ *       Michael Klein
+ */
+
+#include "holberton.h"
 
 unsigned int convert_x(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
