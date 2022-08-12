c78f27fc66bcf1b5c03f346d335251b976890cd3
Ben-Gurion Zanus <106863674+Lilformula@users.noreply.github.com>
2022-08-12 12:40:59 +0000

    Update and rename holberton.h to main.h

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
 #include <limits.h>
 #include <stdarg.h>
@@ -113,4 +113,37 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec);
 
-#endif /* _HOLBERTON_H */
+#endif /* _MAIN_H */
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+
+#endif  /* _MAIN_H */
