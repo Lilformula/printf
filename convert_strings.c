b63aca21673dbe2af4cdd1456beb7a2367ed068f
Ben-Gurion Zanus <106863674+Lilformula@users.noreply.github.com>
2022-08-12 12:25:44 +0000

    Update convert_strings.c

--- convert_strings.c
+++ convert_strings.c
@@ -1,4 +1,10 @@
-#include "main.h"
+/*
+ * File: convert_strings.c
+ * Auth: Brennan D Baraban
+ *       Michael Klein
+ */
+
+#include "holberton.h"
 
 unsigned int convert_s(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
